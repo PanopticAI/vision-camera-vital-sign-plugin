1c27a1e374294e84cb6467e48e2d276a1d218ac6
Eddie Lau <eddie@touchutility.com>
2022-08-19 18:52:36 +0800

    try to call the plugin function

--- ios/VisionCameraVitalSignPlugin.m
+++ ios/VisionCameraVitalSignPlugin.m
@@ -1,6 +1,7 @@
 // VisionCameraVitalSignPlugin.m
 
 #import "VisionCameraVitalSignPlugin.h"
+#import "react-native-vital-sign-plugin/VitalSignPlugin.h"
 
 
 @implementation VisionCameraVitalSignPlugin
@@ -9,8 +10,13 @@ RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
 {
+    [VitalSignEngineCoreProxy processVideoFrame:NULL userInfo:NULL];
+    
+    
     // TODO: Implement some actually useful functionality
     callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+    
+    
 }
 
 @end
