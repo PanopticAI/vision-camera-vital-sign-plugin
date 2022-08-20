cd306c1c343930c4f9802b5d551accfbc3209cb8
Eddie Lau <eddie@touchutility.com>
2022-08-20 16:13:38 +0800

    added type to function prototype

--- index.ts
+++ index.ts
@@ -1,8 +1,16 @@
 // main index.js
 
 import type { Frame } from 'react-native-vision-camera'
+// @ts-ignore
+import type { NormalizedFacebox,NormalizedLandmark,GetHealthResult, UserInfo } from 'react-native-vital-sign-plugin'
 
-export function processVideoFrame(frame: Frame, userInfo:any): string[] {
+export interface ProcessVideoFrameResult {
+  facebox?: NormalizedFacebox,
+  faceLandmarks?: NormalizedLandmark[]
+  healthResult?: GetHealthResult
+}
+
+export function processVideoFrame(frame: Frame, userInfo:UserInfo): ProcessVideoFrameResult {
   'worklet'
   // @ts-ignore
   return __vseProcessVideoFrame(frame, userInfo)
