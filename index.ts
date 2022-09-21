1437adfd6a487c2b091b2b790d00115b69995414
Eddie Lau <eddie@touchutility.com>
2022-09-21 14:13:26 +0800

    added scanConditions

--- index.ts
+++ index.ts
@@ -2,9 +2,10 @@
 
 import type { Frame } from 'react-native-vision-camera'
 // @ts-ignore
-import type { NormalizedFacebox,NormalizedLandmark,GetHealthResult, UserInfo } from 'react-native-vital-sign-plugin'
+import type { NormalizedFacebox,NormalizedLandmark,GetHealthResult, UserInfo, ScanConditions } from 'react-native-vital-sign-plugin'
 
 export interface ProcessFrameResult {
+  scanConditions: ScanConditions
   facebox?: NormalizedFacebox
   faceLandmarks?: NormalizedLandmark[]
   healthResult?: GetHealthResult
